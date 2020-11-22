a959576f17ca08c9bf1f25163db10545622e5e1f
ayoubc <ayoubch807@gmail.com>
2020-11-22 10:46:17 +0100

    DSA

--- datastructure/binary-tree/binary_tree.py
+++ datastructure/binary-tree/binary_tree.py
@@ -5,14 +5,14 @@ class BinaryTree:
         self.right = right
 
     def insert_left(self, val):
-        if self.left == None:
+        if self.left is None:
             self.left = BinaryTree(val)
         else:
             tmp = BinaryTree(val, self.left)
             self.left = tmp
 
     def insert_right(self, val):
-        if self.right == None:
+        if self.right is None:
             self.right = BinaryTree(val)
         else:
             tmp = BinaryTree(val, None, self.right)
@@ -30,27 +30,26 @@ class BinaryTree:
     def get_root_val(self):
         return self.key
 
-
     def __str__(self):
         return self.key
 
 
-
-
 def preorder(tree):
-    if tree:
+    if tree is not None:
         print(tree.get_root_val())
         preorder(tree.get_left_child())
         preorder(tree.get_right_child())
 
+
 def postorder(tree):
-    if tree != None:
+    if tree is not None:
         postorder(tree.get_left_child())
         postorder(tree.get_right_child())
         print(tree.get_root_val())
 
+
 def inorder(tree):
-    if tree != None:
+    if tree is not None:
         inorder(tree.get_left_child())
         print(tree.get_root_val())
         inorder(tree.get_right_child())
